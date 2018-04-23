1998f1e85cc9010483bd18e87dcc5f1f5d503854
Daniel Delgado <dedd1993@gmail.com>
2018-04-22 23:08:51 -0500

    Restful PlanController added with model

---
+++ app/Http/Controllers/PlanController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Plan;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PlanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $planes = Plan::all();
+        return response($planes, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $plan = Plan::create($request->all());
+        return response($plan, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $plan = Plan::find($id);
+        return response($plan, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $plan = Plan::find($id);
+        $plan->culqui_id = $request->culqui_id;
+        $plan->save();
+        return response($plan, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $plan = Plan::find($id);
+        $plan->delete();
+        return response([
+            'id'=> $plan->id,
+            'deleted'=> true,
+            'message' => "Se eliminó el plan con ID ${plan['id']} con exitosamente."
+        ], 200);
+    }
+}
