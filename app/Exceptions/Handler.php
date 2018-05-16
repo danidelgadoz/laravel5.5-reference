e17448f65b9ed7f3191e99774ac2b1ba4b26fe6e
Daniel Delgado <dedd1993@gmail.com>
2018-05-15 20:14:15 -0500

    change(config): server error as json and custom culqui errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,6 +48,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+//        return parent::render($request, $exception);
+//        return $this->prepareJsonResponse($request, $exception);
+
+        if(json_decode($exception->getMessage(), 10))
+            return response(json_decode($exception->getMessage(), 10), 500);
+        else
+            return $this->prepareJsonResponse($request, $exception);
+
     }
 }
