86bd4776dab3ccfe78f0fe75f00db274537914f7
Daniel Delgado <dedd1993@gmail.com>
2018-07-01 17:36:38 -0500

    update(oauth): CORS enabled for oauth routes

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,7 +25,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        Passport::routes();
+        Passport::routes(null, ['middleware' => [ \Barryvdh\Cors\HandleCors::class ]]);
 //        Passport::tokensExpireIn(now()->addMinutes(1));
     }
 }
