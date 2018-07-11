3011e070e62e9a7351769e04e05501247bb4ede6
Daniel Delgado <dedd1993@gmail.com>
2018-07-11 00:29:51 -0500

    update(giftcard): validate a giftcard and use it

--- app/Http/Controllers/GiftcardController.php
+++ app/Http/Controllers/GiftcardController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Giftcard;
+use App\Delivery;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class GiftcardController extends Controller
 {
@@ -14,7 +16,8 @@ class GiftcardController extends Controller
      */
     public function index()
     {
-        //
+        $giftcards = Giftcard::all();
+        return response($giftcards, 200);
     }
 
     /**
@@ -46,7 +49,7 @@ class GiftcardController extends Controller
      */
     public function show(Giftcard $giftcard)
     {
-        //
+        return response($giftcard, 200);
     }
 
     /**
@@ -81,5 +84,56 @@ class GiftcardController extends Controller
     public function destroy(Giftcard $giftcard)
     {
         //
+    }
+
+    public function validar(Request $request)
+    {
+        $giftcard = Giftcard::where('codigo', $request->codigo)
+//                    ->where('canjeado', null)
+                    ->firstOrFail();
+
+        if ($giftcard->canjeado)
+            return response(['error' => "Giftcard anteriormente canjeado."], 409);
+        else
+            return response(['message' => "Giftcard disponible."], 200);
+
+    }
+
+    public function canjear(Request $request)
+    {
+        $giftcard = Giftcard::where('codigo', $request->codigo)->firstOrFail();
+
+        if ($giftcard->canjeado) {
+            return response(['error' => "Giftcard anteriormente canjeado."], 409);
+
+        } else {
+            DB::transaction(function () use ($request, $giftcard) {
+                $delivery = new Delivery();
+                $delivery->direccion = $request->entrega_direccion;
+                $delivery->distrito = $request->entrega_distrito;
+                $delivery->referencia = $request->entrega_referencia;
+                $delivery->nombres = $request->entrega_remitente;
+                $delivery->email = $request->entrega_email;
+                $delivery->celular = $request->entrega_celular;
+                $delivery->save();
+
+                $giftcard->delivery_id = $delivery->id;
+                $giftcard->canjeado = date("Y-m-d H:i:s");
+                $giftcard->save();
+            });
+
+            return response([
+                'message' => "Giftcard canjeado con éxito.",
+                'data' => Giftcard::find($giftcard->id, [
+                    'id',
+                    'codigo',
+                    'canjeado',
+                    'meses',
+                    'precio',
+                    'delivery_id',
+                    'plan_id'
+                ])
+            ], 200);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -23,12 +23,15 @@ Route::apiResource('factura', 'FacturaController');
 Route::apiResource('suscripcion', 'SuscripcionController');
 Route::apiResource('compra', 'CompraController');
 Route::apiResource('delivery', 'DeliveryController');
+Route::apiResource('giftcard', 'GiftcardController');
 Route::apiResource('contacto-log', 'ContactoLogController');
 Route::apiResource('mailing-suscripcion', 'MailingSuscripcionController');
 Route::apiResource('reclamacion', 'ReclamacionController');
 Route::get('suscripcion/{suscripcion}/confirm', 'SuscripcionController@confirm');
 Route::get('suscripcion/{suscripcion}/cancel', 'SuscripcionController@cancel');
 Route::get('suscripcion/export/excel', 'SuscripcionController@export');
+Route::post('giftcard/validar', 'GiftcardController@validar');
+Route::put('giftcard/canjear', 'GiftcardController@canjear');
 
 Route::prefix('admin')->middleware('auth:api')->group(function () {
     Route::apiResource('plan', 'PlanController');
